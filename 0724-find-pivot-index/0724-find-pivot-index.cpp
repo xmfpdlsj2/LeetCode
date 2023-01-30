63ff513814e23d3fd1792d1eaa146c2166beedbc
HoJun Sung <sunghojn@naver.com>
2023-01-30 18:29:25 +0900

    Time: 26 ms (76.02%), Space: 31.2 MB (19.32%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) 
+    {
+        int sum {}, leftSum{};
+        for(auto num : nums)
+        {
+            sum += num;
+        }
+
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            if (leftSum == (sum -= nums[i]))
+            {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+};
