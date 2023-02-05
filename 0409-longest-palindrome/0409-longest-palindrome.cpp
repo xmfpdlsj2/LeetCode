e4388f51102540f3865021c6f443b04f0885e905
HoJun Sung <sunghojn@naver.com>
2023-02-05 12:55:08 +0900

    Time: 4 ms (53.10%), Space: 6.8 MB (20.13%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int longestPalindrome(string s) 
+    {
+         std::map<char, int> table{};
+	     int lengthOfPalindrome{};
+         int oneCheck{};
+
+         for (auto& letter : s)
+         {
+             letter = letter - 'a' + 'A';
+             oneCheck++;
+             if (++table[letter] == 2)
+             {
+                 table[letter] = 0;
+                 lengthOfPalindrome += 2;
+                 oneCheck -= 2;
+             }
+         }
+
+         if (oneCheck > 0)
+         {
+             lengthOfPalindrome++;
+         }
+
+         return lengthOfPalindrome;
+    }
+};
