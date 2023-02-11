f839d894c2423a2e83597106719a90bc1f2dbbd5
HoJun Sung <sunghojn@naver.com>
2023-02-11 15:06:23 +0900

    Time: 0 ms (100.00%), Space: 6 MB (73.62%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,29 +2,14 @@ class Solution {
 public:
     int climbStairs(int n) 
     {
-        int prev1{ 1 }, prev2{ 2 }, result{};
+        int prev1{ 1 }, prev2{ 1 };
         
-        if (n < 2)
+        for (int i = 2; i <= n; i++)
         {
-            return 1;
+            int result = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = result;
         }
-        if (n < 3)
-        {
-            return 2;
-        }
-        
-        for (int i = 0; i < (n - 2); i++)
-        {
-            result = prev1 + prev2;
-            if (i % 2 == 0)            
-            {
-                prev1 = result;
-            }
-            else
-            {
-                prev2 = result;
-            }
-        }
-        return result;
+        return prev1;
     }
 };
