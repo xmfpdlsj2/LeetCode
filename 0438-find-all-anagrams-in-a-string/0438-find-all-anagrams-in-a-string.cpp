0e2931fe80767a7aa1b3dbb4e8fd0b7d20d55daf
HoJun Sung <sunghojn@naver.com>
2023-02-12 15:04:52 +0900

    Time: 11 ms (88.55%), Space: 8.8 MB (41.74%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -2,28 +2,31 @@ class Solution {
 public:
     vector<int> findAnagrams(string s, string p) 
     {
-        std::vector<int> comp(26, 0);
-        std::vector<int> win(26, 0);
-        std::vector<int> result{};
-        
+        if (s.length() < p.length()) return {};
+
+        std::vector<int> freq_p(26, 0);
+        std::vector<int> window(26, 0);
+
+        //first window
         for (int i = 0; i < p.length(); i++) 
         {
-            comp[p[i] - 'a']++;
+            freq_p[p[i] - 'a']++;
+            window[s[i] - 'a']++;
         }
 
-        for (int i = 0; i + p.length() - 1 < s.length(); i++)
+        std::vector<int> ans;
+        if (freq_p == window) ans.push_back(0);
+
+        for (int i = p.length(); i < s.length(); i++) 
         {
-            auto winTemp = win;
-            for (int j = 0; j < p.length(); j++)
-            {
-                winTemp[s[i+j] - 'a']++;
-            }
+            window[s[i - p.length()] - 'a']--;
+            window[s[i] - 'a']++;
 
-            if (comp == winTemp)
+            if (freq_p == window) 
             {
-                result.push_back(i);
+                ans.push_back(i - p.length() + 1);
             }
         }
-        return result;
+        return ans;
     }
 };
