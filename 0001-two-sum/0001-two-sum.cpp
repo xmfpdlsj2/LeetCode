3d28f189925a3b91e62415ae54b05429b0e2a66a
HoJun Sung <sunghojn@naver.com>
2023-02-13 17:51:00 +0900

    Time: 554 ms (23.56%), Space: 11.1 MB (16.83%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) 
     {
+        bool isDone{false};
         std::map<int, int> gap{};
         std::vector<int> result{};
 
@@ -9,14 +10,20 @@ public:
 
         for(int i = 1; i < nums.size(); i++)
         {
+            
             for(auto& e : gap)
             {
                 if (e.second == nums[i])
                 {
                     result.push_back(e.first);
                     result.push_back(i);
+                    isDone = true;
                     break;
                 }
+            }
+            if (isDone)
+            {
+                break;
             }
             gap[i] = target - nums[i];
 
