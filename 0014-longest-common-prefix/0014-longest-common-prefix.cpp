bf158b7dbefa8eced6c9378bb4734a0371efedfb
HoJun Sung <sunghojn@naver.com>
2023-02-15 21:48:00 +0900

    Time: 6 ms (47.21%), Space: 9.1 MB (95.16%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) 
+    {
+        int ans = strs[0].length();
+        for (int i = 1; i < strs.size(); i++) 
+        {
+            int j = 0;
+            while (j < strs[i].length() && strs[i][j] == strs[0][j])
+            {
+                j++;
+            }
+            ans = std::min(ans, j);
+        }
+        return strs[0].substr(0, ans);
+    }
+};
