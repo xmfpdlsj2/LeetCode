0ea89669ff57b94c8b026d42410e16e30008cee8
HoJun Sung <sunghojn@naver.com>
2023-02-16 18:08:35 +0900

    Time: 4 ms (38.84%), Space: 6.4 MB (55.21%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isValid(string s) 
+    {
+        std::map<char, char> brPair{};
+        brPair[')'] = '(';
+        brPair['}'] = '{';
+        brPair[']'] = '[';
+
+        std::stack<char> brackets{};
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == ')' || s[i] == '}' || s[i] == ']')
+            {
+                if (brackets.empty())
+                {
+                    return false;
+                }
+                char open = brackets.top();
+
+                if (open != brPair[s[i]])
+                {
+                    return false;
+                }
+                brackets.pop();
+            }
+            else
+            {
+                brackets.push(s[i]);
+            }
+        }
+        return brackets.empty();
+    }
+};
