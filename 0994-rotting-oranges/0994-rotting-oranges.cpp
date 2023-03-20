8af7fdc73f3b53b4a72db6b2ded7cc16af46761a
HoJun Sung <sunghojn@naver.com>
2023-03-20 12:47:42 +0900

    Time: 53 ms (5.51%), Space: 31 MB (5.01%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    bool IsValid(std::vector<std::vector<int>> grid, int m, int n)
+    {
+        if (m < 0 || m >= grid.size() || n < 0 || n >= grid[0].size())
+        {
+            return false;
+        }
+        return true;
+    }
+    int orangesRotting(std::vector<std::vector<int>>& grid)
+    {
+        int freshOrange{}, minute{};
+        std::queue<std::pair<int, int>> rottPoss{};
+
+        for (int i = 0; i < grid.size(); i++)
+        {
+            for (int j = 0; j < grid[0].size(); j++)
+            {
+                if (grid[i][j] == 2)
+                {
+                    rottPoss.push({ i, j });
+                }
+                else if (grid[i][j] == 1)
+                {
+                    freshOrange++;
+                }
+            }
+        }
+
+        if (freshOrange == 0)
+        {
+            return 0;
+        }
+
+        std::vector<int> nM { -1, 0, 1, 0 };
+        std::vector<int> nN { 0, 1, 0, -1 };
+        bool isRott{ false };
+        while (!rottPoss.empty())
+        {
+            int bundle = rottPoss.size();
+            while (bundle--)
+            {
+                auto rp = rottPoss.front();
+                rottPoss.pop();
+                for (int i = 0; i < nM.size(); i++)
+                {
+                    int m = rp.first + nM[i];
+                    int n = rp.second + nN[i];
+                    if (IsValid(grid, m, n) && grid[m][n] == 1)
+                    {
+                        grid[m][n] = 2;
+                        rottPoss.push({ m, n });
+                        freshOrange--;
+                        isRott = true;
+                    }
+                }
+            }
+            if (isRott)
+            {
+                isRott = false;
+                minute++;
+            }
+        }
+
+        return freshOrange != 0 ? -1 : minute;
+    }
+};
