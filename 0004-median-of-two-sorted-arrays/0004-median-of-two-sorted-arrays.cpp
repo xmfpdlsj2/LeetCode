3bb28342c1d1f2d39efbed8481f9e228bc613642
HoJun Sung <sunghojn@naver.com>
2023-03-23 11:04:51 +0900

    Time: 37 ms (78.55%), Space: 89.8 MB (43.14%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -2,17 +2,29 @@ class Solution {
 public:
     double findMedianSortedArrays(std::vector<int>&nums1, std::vector<int>& nums2)
     {
-        std::vector<int> sumSort{};
-        for (auto& n : nums1)
+        int i{}, j{}, lastIndex{ -1 }, n1 = nums1.size(), n2 = nums2.size();
+        std::vector<int> sumSort(n1 + n2);
+
+        while (i < n1 && j < n2)
         {
-            sumSort.push_back(n);
-        }
-        for (auto& n : nums2)
-        {
-            sumSort.push_back(n);
+            if (nums1[i] <= nums2[j])
+            {
+                sumSort[++lastIndex] = nums1[i++];
+            }
+            else
+            {
+                sumSort[++lastIndex] = nums2[j++];
+            }
         }
 
-        std::sort(sumSort.begin(), sumSort.end());
+        while (i < n1)
+        {
+            sumSort[++lastIndex] = nums1[i++];
+        }
+        while (j < n2)
+        {
+            sumSort[++lastIndex] = nums2[j++];
+        }
 
         int size = sumSort.size();
         return size % 2 ? sumSort[size / 2] : (sumSort[(size / 2) - 1] + sumSort[(size / 2)]) / 2.0;
