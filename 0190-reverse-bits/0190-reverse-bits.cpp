014e528691f112ce01c9d0444e8c31d51b1c98c6
HoJun Sung <sunghojn@naver.com>
2023-04-10 22:20:49 +0900

    Time: 3 ms (57.80%), Space: 6.1 MB (38.64%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -2,20 +2,15 @@ class Solution {
 public:
     uint32_t reverseBits(uint32_t n) 
     {
-     	int comp{0b0001};
         uint32_t ret{};
 
-        for (int  i = 0; i < 32; i++)
+        for (int  i = 0; i < 31; i++)
         {
-            auto temp = n & comp;
-            ret = ret | (temp == 1 ? 1 : 0);
-            if (i == 31)
-            {
-                break;
-            }       
+            ret = ret | (n & 1);
             ret = ret << 1;
             n = n >> 1;
         }
-        return ret;   
+        ret = ret | (n & 1);
+        return ret;
     }
 };
