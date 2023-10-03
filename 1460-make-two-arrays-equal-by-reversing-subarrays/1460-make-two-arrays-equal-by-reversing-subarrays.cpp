6858f7bf5eaf23febbd7b497636a1ef3fc892e6f
HoJun Sung <sunghojn@naver.com>
2023-10-03 09:18:08 +0900

    Time: 15 ms (17.58%), Space: 14.8 MB (42.98%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        std::unordered_map<int, int> table{};
-        for (auto& e : target)
-        {
-            table[e]++;
-        }
-        for (auto& e : arr)
-        {
-            table[e]--;
-            if (table[e] < 0) return false;
-        }
-        return true;
+        std::sort(target.begin(), target.end());
+        std::sort(arr.begin(), arr.end());
+        if (target == arr) return true;
+        return false;
     }
 };
