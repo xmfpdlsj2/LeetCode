2c276d67c0a0a58fc45a7f34607eb1569ada876b
HoJun Sung <sunghojn@naver.com>
2023-11-17 08:56:09 +0900

    Time: 127 ms (9.29%), Space: 68 MB (65.84%) - LeetHub

---
+++ 2815-max-pair-sum-in-an-array/2815-max-pair-sum-in-an-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+        int res = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            string first = to_string(nums[i]);
+            std::sort(begin(first),end(first));
+            
+            for(int j = i+1; j < nums.size(); j++)
+            {
+                string sec = to_string(nums[j]);
+                std::sort(begin(sec),end(sec));
+                if(first[first.size()-1] == sec[sec.size()-1])
+                {
+                    res = max(res,nums[i]+nums[j]);
+                }
+            }
+        }
+        
+        return res == 0 ? -1 : res;
+    }
+};
