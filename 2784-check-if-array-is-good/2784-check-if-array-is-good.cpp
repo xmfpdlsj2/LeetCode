43c3b72610e99df63f80b6e99ed66fb26ca4ed6e
HoJun Sung <sunghojn@naver.com>
2023-11-22 08:36:22 +0900

    Time: 11 ms (22.65%), Space: 22.9 MB (94.44%) - LeetHub

---
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        int  n= nums.size();
+        if(n==0 || n==1){
+            return false;
+        }
+        sort(nums.begin(),nums.end());
+        if(nums[n-2]!=nums[n-1]){
+            return false;
+        }
+        for(int i = 0; i<n-1; i++){
+            if(nums[i]!=i+1){
+                return false;
+            }
+        }
+        return true;
+    }
+};
