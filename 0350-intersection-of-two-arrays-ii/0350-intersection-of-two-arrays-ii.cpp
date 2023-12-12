3d8c3d233dcd05c3ad792808eaec10847429cc72
HoJun Sung <sunghojn@naver.com>
2023-12-12 10:41:41 +0900

    Time: 0 ms (100.00%), Space: 11 MB (21.72%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        if(nums1.size() > nums2.size()) 
+        {
+            swap(nums1, nums2);
+        }
+        
+        unordered_map<int, int> map;
+        for(auto val: nums1) 
+        {
+            map[val]++;
+        }
+        
+        int idx = 0;
+        for(auto val: nums2) 
+        {
+            if(map[val] > 0)
+            {
+                nums1[idx++] = val;
+                map[val]--;
+            }
+        }
+        return vector<int>(nums1.begin(), nums1.begin() + idx);    
+    }
+};
