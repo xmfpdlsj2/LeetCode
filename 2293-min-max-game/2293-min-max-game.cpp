a9b355302433c1651b80ad87d02774dc402a959f
HoJun Sung <sunghojn@naver.com>
2024-01-07 14:37:06 +0900

    Time: 4 ms (79.88%), Space: 10.6 MB (8.58%) - LeetHub

---
+++ 2293-min-max-game/2293-min-max-game.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minMaxGame(vector<int>& nums) {
+          while(nums.size()!=1){
+         vector<int>currvect;
+         bool flag = true;
+         for(int i=0;i<nums.size()-1;i++){
+           if(flag){
+              currvect.push_back(min(nums[i],nums[i+1]));
+               flag = false;
+           }
+           else{
+               currvect.push_back(max(nums[i],nums[i+1]));
+               flag = true;
+           }
+           i++;
+         }
+         nums = currvect;
+      }
+     return nums[0];
+    }
+};
