a1afa4ff1b8f2c2c656da4f09aa7c9d4928ed02d
HoJun Sung <sunghojn@naver.com>
2024-01-12 09:23:26 +0900

    Time: 4 ms (28.89%), Space: 6.8 MB (5.53%) - LeetHub

---
+++ 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool digitCount(string num) {
+        unordered_map<int,int> mpp;
+        for(const auto& i : num)
+        {
+            mpp[i-'0']++;
+        }
+        for(int i = 0; i < num.size(); i++)
+        {
+            if(num[i] - '0' != mpp[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
