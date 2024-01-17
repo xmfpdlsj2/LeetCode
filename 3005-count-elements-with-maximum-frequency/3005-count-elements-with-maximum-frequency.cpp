76bf1ed1f5a0df60e1f65d442099c014a175e8c3
HoJun Sung <sunghojn@naver.com>
2024-01-18 08:53:18 +0900

    Time: 4 ms (70.85%), Space: 19.6 MB (46.26%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
@@ -3,11 +3,15 @@ public:
     int maxFrequencyElements(vector<int>& nums) {
         vector<int> v(101,0);
         int k = 0, ans = 0;
-        for(auto &i: nums){
+        for(auto &i: nums)
+        {
             v[i]++;
-            if(v[i]>k){
+            if(v[i]>k)
+            {
                 k = v[i],ans = v[i];
-            }else if(v[i]==k){
+            }
+            else if(v[i]==k)
+            {
                 ans += v[i];
             }
         }
