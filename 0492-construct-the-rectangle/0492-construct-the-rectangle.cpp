fb736510dd93e8b1e6322e100a357ad9bf7c39e3
HoJun Sung <sunghojn@naver.com>
2024-03-26 23:28:02 +0900

    Time: 0 ms (100.00%), Space: 7.2 MB (60.40%) - LeetHub

---
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> constructRectangle(int area) {
+        int m = sqrt(area);
+        while (area % m != 0){
+            m--;
+        }
+        return {area/m,m};
+    }
+};
